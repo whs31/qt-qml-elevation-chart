2bc6f068dd36a30c0712feacbe8fe309b20df51c
Рязанцев Дмитрий <ryazancev_dl@uav.radar-mms.com>
2023-04-06 13:07:51 +0300

    GLSL files

--- src/scenegraph/glpolygonshader.hpp
+++ src/scenegraph/glpolygonshader.hpp
@@ -1,70 +1,51 @@
 #pragma once
 
 #include <QtQuick/qsgsimplematerial.h>
+#include <QFile>
 
 struct State
 {
     QColor color;
-
-    int compare(const State *other) const {
-        uint rgb = color.rgba();
-        uint otherRgb = other->color.rgba();
-
-        if (rgb == otherRgb) {
-            return 0;
-        } else if (rgb < otherRgb) {
-            return -1;
-        } else {
-            return 1;
-        }
+    int compare(const State* other) const {
+        if (color.rgba() == other->color.rgba()) return 0;
+        else if (color.rgba() < other->color.rgba()) return -1;
+        else return 1;
     }
 };
 
 class GLPolygonShader : public QSGSimpleMaterialShader<State>
 {
     QSG_DECLARE_SIMPLE_COMPARABLE_SHADER(GLPolygonShader, State)
-//! [2] //! [3]
-public:
 
-    const char *vertexShader() const override {
-        return
-                "attribute highp vec4 aVertex;                              \n"
-                "attribute highp vec2 aTexCoord;                            \n"
-                "uniform highp mat4 qt_Matrix;                              \n"
-                "varying highp vec2 texCoord;                               \n"
-                "void main() {                                              \n"
-                "    gl_Position = qt_Matrix * aVertex;                     \n"
-                "    texCoord = aTexCoord;                                  \n"
-                "}";
-    }
-
-    const char *fragmentShader() const override {
-        return
-                "uniform lowp float qt_Opacity;                             \n"
-                "uniform lowp vec4 color;                                   \n"
-                "varying highp vec2 texCoord;                               \n"
-                "void main ()                                               \n"
-                "{                                                          \n"
-                "    gl_FragColor = texCoord.y * texCoord.x * color * qt_Opacity;  \n"
-                "}";
-    }
-//! [3] //! [4]
-    QList<QByteArray> attributes() const override
-    {
-        return QList<QByteArray>() << "aVertex" << "aTexCoord";
-    }
-//! [4] //! [5]
-    void updateState(const State *state, const State *) override
-    {
-        program()->setUniformValue(id_color, state->color);
-    }
-//! [5] //! [6]
-    void resolveUniforms() override
-    {
-        id_color = program()->uniformLocation("color");
-    }
-
-private:
     int id_color;
-//! [6]
+
+    public:
+        const char *vertexShader() const override {
+            QFile file(":/polygon.vert");
+            if(not file.open(QIODevice::ReadOnly | QIODevice::Text))
+                return "Error reading vertex shader from file";
+            return file.readAll().constData();
+        }
+
+        const char *fragmentShader() const override {
+            QFile file(":/polygon.frag");
+            if(not file.open(QIODevice::ReadOnly | QIODevice::Text))
+                return "Error reading fragment shader from file";
+            return file.readAll().constData();
+        }
+
+        QList<QByteArray> attributes() const override
+        {
+            return QList<QByteArray>() << "aVertex" << "aTexCoord";
+        }
+
+        void updateState(const State *state, const State *) override
+        {
+            program()->setUniformValue(id_color, state->color);
+        }
+
+        void resolveUniforms() override
+        {
+            id_color = program()->uniformLocation("color");
+        }
 };
