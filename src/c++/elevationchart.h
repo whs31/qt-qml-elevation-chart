e800d9d201a0a5c4cf8a8d9e4aff150d103a6626
whs31 <75864292+whs31@users.noreply.github.com>
2023-08-08 15:56:52 +0300

    Update elevationchart.h

--- src/c++/elevationchart.h
+++ src/c++/elevationchart.h
@@ -15,17 +15,30 @@ namespace ElevationChart
   {
     Q_OBJECT
     Q_PROPERTY(SG::BasicPalette palette READ palette WRITE setPalette NOTIFY paletteChanged FINAL)
+    Q_PROPERTY(ElevationChart::Route route READ route WRITE setRoute NOTIFY routeChanged FINAL)
+    Q_PROPERTY(QGeoCoordinate uavPosition READ uavPosition WRITE setUavPosition NOTIFY uavPositionChanged FINAL)
+
+    // metrics gadget
+
+    // envelope gadget
+
     Q_PROPERTY(bool intersecting READ intersecting WRITE setIntersecting NOTIFY intersectingChanged FINAL)
     Q_PROPERTY(bool valid READ valid WRITE setValid NOTIFY validChanged FINAL)
-    Q_PROPERTY(ElevationChart::Route route READ route WRITE setRoute NOTIFY routeChanged FINAL)
+    // matching metrics
+    // is busy
 
     public:
       explicit ChartItem(QQuickItem* parent = nullptr);
 
       [[nodiscard]] SG::BasicPalette palette() const; void setPalette(SG::BasicPalette);
+      [[nodiscard]] Route route() const;              void setRoute(const Route&);
+
       [[nodiscard]] bool intersecting() const;        void setIntersecting(bool);
       [[nodiscard]] bool valid() const;               void setValid(bool);
-      [[nodiscard]] Route route() const;              void setRoute(const Route&);
+
+      Q_INVOKABLE void applyMetricsCorrection() noexcept;
+      Q_INVOKABLE void estimateEnvelope() noexcept;
+      Q_INVOKABLE void applyEnvelopeCorrection() noexcept;
 
     signals:
       void paletteChanged();
