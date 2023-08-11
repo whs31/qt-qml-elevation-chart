9c7921f81ef3f638a99ac630140fd3aa1711648b
whs31 <75864292+whs31@users.noreply.github.com>
2023-08-11 12:11:22 +0300

    Update metrics.h

--- src/c++/types/metrics.h
+++ src/c++/types/metrics.h
@@ -33,24 +33,74 @@ Q_DECLARE_METATYPE(ElevationChart::Metrics)
 
 namespace ElevationChart
 {
+  /**
+   * \class Metrics
+   * \brief Класс-гаджет, хранящий входные данные для расчета коррекции по ЛТХ.
+   * \note Класс зарегистрирован как гаджет и может использоваться в качестве Q_PROPERTY
+   * как в C++, так и в QML.
+   */
+
+  /**
+   * \brief Создает объект Metrics со значениями по умолчанию.
+   * \details Значения по умолчанию: <tt>[10, 10, 10]</tt>.
+   */
   inline Metrics::Metrics()
     : m_rateOfClimb(10.0f)
     , m_rateOfDescend(10.0f)
     , m_fallbackVelocity(10.0f)
   {}
 
+  /**
+   * \brief Создает объект Metrics с указанными значениями.
+   * \param roc - скороподъемность в м/с.
+   * \param rod - скорость спуска в м/с.
+   * \param vh - горизонтальная скорость в м/с. Это значение будет использоваться только в том
+   * случае, если точка в пути Route не имеет собственного значения скорости.
+   */
   inline Metrics::Metrics(float roc, float rod, float vh)
     : m_rateOfClimb(roc)
     , m_rateOfDescend(rod)
     , m_fallbackVelocity(vh)
   {}
 
+  /**
+   * \property Bounds::rateOfClimb
+   * \brief Скороподъемность БПЛА в м/с.
+   * \details
+   * <table>
+   * <caption id="multi_row">Связанные функции</caption>
+   * <tr><th>Чтение                 <th>Запись                  <th>Оповещение
+   * <tr><td><i>rateOfClimb</i>     <td><i>setRateOfClimb</i>   <td><i>--</i>
+   * </table>
+   */
   inline float Metrics::rateOfClimb() const { return m_rateOfClimb; }
   inline void Metrics::setRateOfClimb(float x) { m_rateOfClimb = x; }
 
+  /**
+   * \property Bounds::rateOfDescend
+   * \brief Скорость спуска БПЛА в м/с.
+   * \details
+   * <table>
+   * <caption id="multi_row">Связанные функции</caption>
+   * <tr><th>Чтение                 <th>Запись                  <th>Оповещение
+   * <tr><td><i>rateOfDescend</i>   <td><i>setRateOfDescend</i> <td><i>--</i>
+   * </table>
+   */
   inline float Metrics::rateOfDescend() const { return m_rateOfDescend; }
   inline void Metrics::setRateOfDescend(float x) { m_rateOfDescend = x; }
 
+  /**
+   * \property Bounds::fallbackVelocity
+   * \brief Горизонтальная скорость в м/с.
+   * \details
+   * Это значение будет использоваться только в том
+   * случае, если точка в пути Route не имеет собственного значения скорости.
+   * <table>
+   * <caption id="multi_row">Связанные функции</caption>
+   * <tr><th>Чтение                 <th>Запись                     <th>Оповещение
+   * <tr><td><i>fallbackVelocity</i><td><i>setFallbackVelocity</i> <td><i>--</i>
+   * </table>
+   */
   inline float Metrics::fallbackVelocity() const { return m_fallbackVelocity; }
   inline void Metrics::setFallbackVelocity(float x) { m_fallbackVelocity = x; }
 } // ElevationChart
